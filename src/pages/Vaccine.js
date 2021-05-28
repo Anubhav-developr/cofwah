1efefdf82adcb7ee0bc5b048c11b833c179216d6
Anubhav-developr <anubhavm1234@gmail.com>
2021-05-29 01:35:33 +0530

    Update Vaccine.js

--- src/pages/Vaccine.js
+++ src/pages/Vaccine.js
@@ -2,12 +2,15 @@ import React , { useState } from 'react';
 import MoviesList from '../components/MoviesList';
 
 function Vaccine(){
+    ipp=0;
+    var ipp = +document.getElementById("t").value;
+    var s = ipp.toString();
 
    //vaccine by district trial 
    const [movies, setMovies] = useState([]);
 
  async function fetchMoviesHandler() {
-    const response = await fetch('https://cdn-api.co-vin.in/api/v2/admin/location/districts/24');
+    const response = await fetch('https://cdn-api.co-vin.in/api/v2/admin/location/districts/'+s);
     const data = await response.json()
       
         const transformedata = data.districts.map((vaxinedata) => {
@@ -36,7 +39,9 @@ function Vaccine(){
 
 
 
-return( 
+return( <React.Fragment>
+        <input type="Number" id="t"></input>
+        <button>Submit state id</button>
        <p> <button onClick={fetchMoviesHandler}>District List with Id</button> <br></br> <br></br>
         <p>  <MoviesList movies={movies} /></p>
 
@@ -44,6 +49,7 @@ return(
         <p>  <MoviesList movies={movies} /></p>
         
         </p> </p>
+        </React.Fragment>
     
 );
 
