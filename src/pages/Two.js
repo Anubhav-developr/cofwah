f1d5404a296d4679ba8bd3833f884c664739e3ae
Anubhav-developr <anubhavm1234@gmail.com>
2021-05-31 15:01:39 +0530

    m

--- src/pages/Two.js
+++ src/pages/Two.js
@@ -2,7 +2,7 @@
 function Two(){
     return(
  <div className="csskabaap">
-    <div class="r"><h3 class="w">There is <span>NO</span> known Linkage Between Blood Type and Coronavirus </h3>
+    <div class="r"><h3 class="w">रक्त प्रकार और कोरोनावायरस के बीच कोई ज्ञात संबंध <span>नहीं</span> है</h3>
     <h4 id="q"></h4></div>
  </div>
     );
